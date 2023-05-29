88755602bfe03c909a1f857b8bebc65b3c954fcb
Jawad Ali <jawad.ali@repairdesk.co>
2023-05-29 11:44:35 +0500

    made forecast item card

---
+++ src/components/ForcastItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <ion-item lines="none">
+    <ion-avatar slot="start">
+      <img
+        src="https://images.unsplash.com/reserve/Af0sF2OS5S5gatqrKzVP_Silhoutte.jpg?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dGhlJTIwc3VuJTIwaWNvbnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60">
+    </ion-avatar>
+    <ion-label>
+      <h2>42° F</h2>
+      <h3 style="color: #555;">Clear Sky</h3>
+    </ion-label>
+    <p style="color: #666;">Sat, May 2023</p>
+  </ion-item>
+</template>
+
+<script setup lang="ts">
+import { IonItem, IonLabel } from '@ionic/vue';
+
+</script>
+<style scoped></style>
