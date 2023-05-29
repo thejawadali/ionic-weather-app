a89e88f418e95dcd4b68c1ae6c8a132503114704
Jawad Ali <jawad.ali@repairdesk.co>
2023-05-29 11:44:53 +0500

    removed routing for now

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,31 @@
 <template>
-  <ion-app>
-    <ion-router-outlet />
-  </ion-app>
+  <WeatherCard></WeatherCard>
+  <ion-list>
+    <ion-list-header>
+      <ion-label>
+        <h1>Forecast</h1>
+      </ion-label>
+    </ion-list-header>
+    <ForcastItem v-for="i in 3" :key="i" />
+  </ion-list>
 </template>
 
 <script setup lang="ts">
-import { IonApp, IonRouterOutlet } from '@ionic/vue'
+import WeatherCard from "./components/WeatherCard.vue"
+import ForcastItem from "./components/ForcastItem.vue"
+import { IonList } from '@ionic/vue'
+import { useWeatherStore } from "./store/weather"
+import { onMounted } from "vue"
+
+const weatherStore = useWeatherStore()
+
+onMounted(async () => {
+  await weatherStore.getWeather()
+})
 </script>
+
+<style scoped>
+ion-list {
+  max-width: 350px;
+}
+</style>
